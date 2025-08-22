65eacc1879c41e92f643257a5a6895e380be3276
Hannah Hadfield <skykittenpuppy@beegirl.gay>
2025-08-22 08:38:37 +0100

    First Commit

---
+++ src/client/java/gay/beegirl/SkysSkyIslandsClient.java
@@ -0,0 +1,10 @@
+package gay.beegirl;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkysSkyIslandsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
