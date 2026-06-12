c23c3437f75c29fde8d5b6fbfa49da12f7a48bf0
Hannah Hadfield <skykittenpuppy@beegirl.gay>
2026-06-12 01:45:25 +0100

    Neoforge First Commit

--- src/main/java/gay/beegirl/entity/StaminaHolder.java
+++
@@ -1,8 +0,0 @@
-package gay.beegirl.entity;
-
-public interface StaminaHolder {
-    default float getStaminaHideDelay() { return 1; }
-    int getStamina();
-    int getMaxStamina();
-    void setStamina(int stamina);
-}
