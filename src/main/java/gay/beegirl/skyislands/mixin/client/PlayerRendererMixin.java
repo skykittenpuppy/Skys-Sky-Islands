b6d23ffcc0186545c421e7b83459fc33e79ba899
Ori <redraino.o.o.o.o@gmail.com>
2026-06-15 15:17:40 -0400

    attempt to force hand rendering while gliding (failed) (sky please review and figure out)

--- src/main/java/gay/beegirl/skyislands/mixin/client/PlayerRendererMixin.java
+++ src/main/java/gay/beegirl/skyislands/mixin/client/PlayerRendererMixin.java
@@ -8,7 +8,9 @@ import gay.beegirl.skyislands.client.GliderLayer;
 import gay.beegirl.skyislands.entity.ModDataAttachments;
 import gay.beegirl.skyislands.util.LivingEntityAccess;
 import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
@@ -16,12 +18,16 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerRenderer.class)
 public abstract class PlayerRendererMixin extends LivingEntityRenderer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
+    @Shadow
+    protected abstract void renderHand(PoseStack poseStack, MultiBufferSource buffer, int combinedLight, AbstractClientPlayer player, ModelPart rendererArm, ModelPart rendererArmwear);
+
     public PlayerRendererMixin(EntityRendererProvider.Context context, PlayerModel<AbstractClientPlayer> model, float shadowRadius) {
         super(context, model, shadowRadius);
     }
@@ -30,6 +36,14 @@ public abstract class PlayerRendererMixin extends LivingEntityRenderer<AbstractC
     private void islands$init(EntityRendererProvider.Context context, boolean useSlimModel, CallbackInfo ci) {
         this.addLayer(new GliderLayer(this, context.getModelSet()));
     }
+
+    /*@Inject(method ="renderRightHand", at = @At("HEAD"), cancellable = true)
+    private void islands$renderFirstPersonHandWhileGliding(PoseStack poseStack, MultiBufferSource buffer, int combinedLight, AbstractClientPlayer player, CallbackInfo ci) {
+        if (player.getData(ModDataAttachments.IS_GLIDING)) {
+            this.renderHand(poseStack, buffer, combinedLight, player, ((PlayerModel)this.model).leftArm, ((PlayerModel)this.model).leftSleeve);
+            ci.cancel();
+        }
+    }*/
 
     @WrapOperation(method = "setupRotations(Lnet/minecraft/client/player/AbstractClientPlayer;Lcom/mojang/blaze3d/vertex/PoseStack;FFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/LivingEntityRenderer;setupRotations(Lnet/minecraft/world/entity/LivingEntity;Lcom/mojang/blaze3d/vertex/PoseStack;FFFF)V", ordinal = 2))
     private void islands$setupRotations(PlayerRenderer instance, LivingEntity entity, PoseStack poseStack, float bob, float yBodyRot, float partialTick, float scale, Operation<Void> original) {
