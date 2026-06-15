8a362de4f74272f66894306c41d2948381a500af
Ori <redraino.o.o.o.o@gmail.com>
2026-06-15 15:26:12 -0400

    disable attacking while gliding, reorginize glider anims, and start an attempt at arm anims

--- src/main/java/gay/beegirl/skyislands/mixin/PlayerMixin.java
+++ src/main/java/gay/beegirl/skyislands/mixin/PlayerMixin.java
@@ -10,11 +10,18 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity {
     protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
         super(entityType, level);
+    }
+
+    @Inject(method = "attack", at = @At(value = "HEAD"), cancellable = true)
+    private void islands$dontAttackWhileGliding(CallbackInfo ci) {
+        if (this.getData(ModDataAttachments.IS_GLIDING)) ci.cancel();
     }
 
     @WrapOperation(method = "updatePlayerPose", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setPose(Lnet/minecraft/world/entity/Pose;)V", ordinal = 1))
