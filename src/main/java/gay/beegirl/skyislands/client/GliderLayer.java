2b1ec6fc704a91de2809418640bfd1736d38392e
Ori <redraino.o.o.o.o@gmail.com>
2026-06-17 03:54:00 -0400

    glider uses 2 textures

--- src/main/java/gay/beegirl/skyislands/client/GliderLayer.java
+++ src/main/java/gay/beegirl/skyislands/client/GliderLayer.java
@@ -2,6 +2,7 @@ package gay.beegirl.skyislands.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import gay.beegirl.skyislands.SkysSkyIslands;
 import gay.beegirl.skyislands.entity.ModDataAttachments;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.EntityModelSet;
@@ -20,13 +21,17 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 
 public class GliderLayer extends RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
-    public static final ResourceLocation TEXTURE = ResourceLocation.withDefaultNamespace("textures/entity/trident_riptide.png");//SkysSkyIslands.createId("textures/entity/termite/binder.png");
+    public static final ResourceLocation TEXTURE = SkysSkyIslands.createId("textures/entity/glider/frame.png");
+    public static final ResourceLocation CLOTH_TEXTURE = SkysSkyIslands.createId("textures/entity/glider/default.png");
     private final ModelPart box;
+    private final ModelPart clothRoot;
 
     public GliderLayer(RenderLayerParent<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> renderer, EntityModelSet modelSet) {
         super(renderer);
-        ModelPart modelpart = modelSet.bakeLayer(ModModelLayers.GLIDER);
-        this.box = modelpart.getChild("box");
+        ModelPart modelpartFrame = modelSet.bakeLayer(ModModelLayers.GLIDER);
+        ModelPart modelpartCloth = modelSet.bakeLayer(ModModelLayers.GLIDER_CLOTH);
+        this.box = modelpartFrame.getChild("box");
+        this.clothRoot = modelpartCloth.getChild("cloth_root");
     }
 
     public static LayerDefinition createLayer() {
@@ -36,11 +41,22 @@ public class GliderLayer extends RenderLayer<AbstractClientPlayer, PlayerModel<A
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
 
+    public static LayerDefinition createClothLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+        /*PartDefinition clothRootModel = */partdefinition.addOrReplaceChild("cloth_root", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -13.0F, -8.0F, 16.0F, 3.0F, 16.0F), PartPose.ZERO);
+        //boxModel.addOrReplaceChild("box", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, -8.0F, 16.0F, 3.0F, 16.0F), PartPose.ZERO);
+        //boxModel.addOrReplaceChild("box", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, -8.0F, 16.0F, 3.0F, 16.0F), PartPose.ZERO);
+        return LayerDefinition.create(meshdefinition, 64, 64);
+    }
+
     @Override
     public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, AbstractClientPlayer abstractClientPlayer, float v, float v1, float v2, float v3, float v4, float v5) {
         if (abstractClientPlayer.getData(ModDataAttachments.IS_GLIDING)) {
-            VertexConsumer vertexconsumer = multiBufferSource.getBuffer(RenderType.entityCutoutNoCull(TEXTURE));
-            this.box.render(poseStack, vertexconsumer, i, OverlayTexture.NO_OVERLAY);
+            VertexConsumer hvertexconsumer = multiBufferSource.getBuffer(RenderType.entityCutoutNoCull(TEXTURE));
+            this.box.render(poseStack, hvertexconsumer, i, OverlayTexture.NO_OVERLAY);
+            VertexConsumer cvertexconsumer = multiBufferSource.getBuffer(RenderType.entityCutoutNoCull(CLOTH_TEXTURE));
+            this.clothRoot.render(poseStack, cvertexconsumer, i, OverlayTexture.NO_OVERLAY);
         }
     }
 }
--- src/main/java/gay/beegirl/skyislands/client/ModModelLayers.java
+++ src/main/java/gay/beegirl/skyislands/client/ModModelLayers.java
@@ -13,7 +13,8 @@ import java.util.Set;
 public class ModModelLayers {
     private static final String DEFAULT_LAYER = "main";
     private static final Set<ModelLayerLocation> ALL_MODELS = Sets.newHashSet();
-    public static final ModelLayerLocation GLIDER = register("gider");
+    public static final ModelLayerLocation GLIDER = register("glider");
+    public static final ModelLayerLocation GLIDER_CLOTH = register("glider", "cloth");
 
     private static ModelLayerLocation register(String path) {
         return register(path, "main");
@@ -36,5 +37,6 @@ public class ModModelLayers {
     public static void registerModelLayers(EntityRenderersEvent.RegisterLayerDefinitions modEventBus) {
         SkysSkyIslands.LOGGER.info("Registering Model Layers for " + SkysSkyIslands.MOD_ID);
         modEventBus.registerLayerDefinition(GLIDER, GliderLayer::createLayer);
+        modEventBus.registerLayerDefinition(GLIDER_CLOTH, GliderLayer::createClothLayer);
     }
 }
