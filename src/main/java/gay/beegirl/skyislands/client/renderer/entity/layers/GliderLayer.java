fbd3c87cb6e0db91e875e8960ddd0eac562d1136
Hannah Hadfield <skykittenpuppy@beegirl.gay>
2026-06-30 10:53:20 +0100

    Glider Visual Changes And Shit

--- src/main/java/gay/beegirl/skyislands/client/renderer/entity/layers/GliderLayer.java
+++ src/main/java/gay/beegirl/skyislands/client/renderer/entity/layers/GliderLayer.java
@@ -14,10 +14,7 @@ import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.CubeListBuilder;
-import net.minecraft.client.model.geom.builders.LayerDefinition;
-import net.minecraft.client.model.geom.builders.MeshDefinition;
-import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
@@ -31,30 +28,85 @@ import net.minecraft.world.phys.Vec3;
 public class GliderLayer extends RenderLayer<LivingEntity, HumanoidModel<LivingEntity>> {
     public static final ResourceLocation FRAME_TEXTURE = SkysSkyIslands.createId("textures/entity/glider/frame.png");
     public static final ResourceLocation DEFAULT_CLOTH_TEXTURE = SkysSkyIslands.createId("textures/designs/entity/glider/default.png");
-    private final ModelPart frameRoot;
-    private final ModelPart clothRoot;
+    //private final ModelPart frameRoot;
+    //private final ModelPart clothRoot;
+    private final ModelPart LEFT_FRAME;
+    private final ModelPart RIGHT_FRAME;
+    private final ModelPart LEFT_CLOTH;
+    private final ModelPart RIGHT_CLOTH;
+    private final ModelPart LEFT_CLOTH_STREAMER;
+    private final ModelPart RIGHT_CLOTH_STREAMER;
 
     public GliderLayer(RenderLayerParent<LivingEntity, HumanoidModel<LivingEntity>> renderer, EntityModelSet modelSet) {
         super(renderer);
         ModelPart modelPartFrame = modelSet.bakeLayer(ModModelLayers.GLIDER);
         ModelPart modelPartCloth = modelSet.bakeLayer(ModModelLayers.GLIDER_CLOTH);
-        this.frameRoot = modelPartFrame.getChild("frame_root");
-        this.clothRoot = modelPartCloth.getChild("cloth_root");
+        //this.frameRoot = modelPartFrame.getChild("frame_root");
+        //this.clothRoot = modelPartCloth.getChild("cloth_root");
+
+        this.LEFT_FRAME = modelPartFrame.getChild("LEFT_FRAME");
+        this.RIGHT_FRAME = modelPartFrame.getChild("RIGHT_FRAME");
+        this.LEFT_CLOTH = modelPartCloth.getChild("LEFT_CLOTH");
+        this.RIGHT_CLOTH = modelPartCloth.getChild("RIGHT_CLOTH");
+        this.LEFT_CLOTH_STREAMER = modelPartCloth.getChild("LEFT_CLOTH_STREAMER");
+        this.RIGHT_CLOTH_STREAMER = modelPartCloth.getChild("RIGHT_CLOTH_STREAMER");
     }
 
     public static LayerDefinition createLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
-        PartDefinition franeRoot = partdefinition.addOrReplaceChild("frame_root", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, -8.0F, 16.0F, 3.0F, 16.0F), PartPose.ZERO);
-        franeRoot.addOrReplaceChild("left_handle", CubeListBuilder.create().texOffs(0, 0).addBox(-7.5F, -8.0F, -4.0F, 2.0F, 2.0F, 8.0F), PartPose.ZERO);
-        franeRoot.addOrReplaceChild("right_handle", CubeListBuilder.create().texOffs(0, 0).addBox(5.5F, -8.0F, -4.0F, 2.0F, 2.0F, 8.0F), PartPose.ZERO);
+        //PartDefinition franeRoot = partdefinition.addOrReplaceChild("frame_root", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, -8.0F, 16.0F, 3.0F, 16.0F), PartPose.ZERO);
+        //franeRoot.addOrReplaceChild("left_handle", CubeListBuilder.create().texOffs(0, 0).addBox(-7.5F, -8.0F, -4.0F, 2.0F, 2.0F, 8.0F), PartPose.ZERO);
+        //franeRoot.addOrReplaceChild("right_handle", CubeListBuilder.create().texOffs(0, 0).addBox(5.5F, -8.0F, -4.0F, 2.0F, 2.0F, 8.0F), PartPose.ZERO);
+
+        PartDefinition LEFT_FRAME = partdefinition.addOrReplaceChild("LEFT_FRAME", CubeListBuilder.create()
+                .texOffs(0, 0).addBox(0.0F, 0.0F, -11.0F, 16.0F, 4.0F, 4.0F, CubeDeformation.NONE)
+                .texOffs(0, 16).addBox(7.0F, 1.0F, -12.0F, 2.0F, 2.0F, 12.0F, CubeDeformation.NONE)
+                .texOffs(32, 16).addBox(7.0F, 1.0F, 0.0F, 2.0F, 2.0F, 12.0F, CubeDeformation.NONE)
+                .texOffs(0, 8).addBox(0.0F, 0.0F, 7.0F, 12.0F, 4.0F, 4.0F, CubeDeformation.NONE)
+                .texOffs(34, 8).addBox(7.0F, 5.0F, -3.0F, 2.0F, 2.0F, 6.0F, CubeDeformation.NONE)
+                .texOffs(32, 8).addBox(7.0F, 3.0F, -5.0F, 2.0F, 4.0F, 2.0F, CubeDeformation.NONE)
+                .texOffs(44, 8).addBox(7.0F, 3.0F, 3.0F, 2.0F, 4.0F, 2.0F, CubeDeformation.NONE),
+                PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, 0.0F, 0.0F, 0.1745F));
+
+        PartDefinition RIGHT_FRAME = partdefinition.addOrReplaceChild("RIGHT_FRAME", CubeListBuilder.create()
+                .texOffs(0, 32).addBox(-16.0F, 0.0F, -11.0F, 16.0F, 4.0F, 4.0F, CubeDeformation.NONE)
+                .texOffs(0, 48).addBox(-9.0F, 1.0F, -12.0F, 2.0F, 2.0F, 12.0F, CubeDeformation.NONE)
+                .texOffs(32, 48).addBox(-9.0F, 1.0F, 0.0F, 2.0F, 2.0F, 12.0F, CubeDeformation.NONE)
+                .texOffs(0, 40).addBox(-12.0F, 0.0F, 7.0F, 12.0F, 4.0F, 4.0F, CubeDeformation.NONE)
+                .texOffs(34, 40).addBox(-9.0F, 5.0F, -3.0F, 2.0F, 2.0F, 6.0F, CubeDeformation.NONE)
+                .texOffs(32, 40).addBox(-9.0F, 3.0F, -5.0F, 2.0F, 4.0F, 2.0F, CubeDeformation.NONE)
+                .texOffs(44, 40).addBox(-9.0F, 3.0F, 3.0F, 2.0F, 4.0F, 2.0F, CubeDeformation.NONE),
+                PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, 0.0F, 0.0F, -0.1745F));
+
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
 
     public static LayerDefinition createClothLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
-        PartDefinition clothRootModel = partdefinition.addOrReplaceChild("cloth_root", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -13.0F, -8.0F, 16.0F, 3.0F, 16.0F), PartPose.ZERO);
+        //PartDefinition clothRootModel = partdefinition.addOrReplaceChild("cloth_root", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -13.0F, -8.0F, 16.0F, 3.0F, 16.0F), PartPose.ZERO);
+
+        PartDefinition LEFT_CLOTH = partdefinition.addOrReplaceChild("LEFT_CLOTH", CubeListBuilder.create()
+                .texOffs(-22, 0).addBox(0.0F, 0.0F, -11.0F, 16.0F, 0.0F, 22.0F, CubeDeformation.NONE)
+                .texOffs(0, 48).addBox(1.0F, 0.0F, -11.0F, 14.0F, 2.0F, 0.0F, CubeDeformation.NONE)
+                .texOffs(0, 50).addBox(1.0F, 0.0F, 11.0F, 10.0F, 2.0F, 0.0F, CubeDeformation.NONE),
+                PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, 0.0F, 0.0F, 0.1745F));
+
+        PartDefinition LEFT_CLOTH_STREAMER = partdefinition.addOrReplaceChild("LEFT_CLOTH_STREAMER", CubeListBuilder.create()
+                .texOffs(-16, 22).addBox(1.0F, 0.0F, 0.0F, 10.0F, 0.0F, 16.0F, CubeDeformation.NONE),
+                PartPose.offsetAndRotation(0.0F, 0.0F, 11.0F, -0.1745F, 0.0F, 0.0F));
+
+        PartDefinition RIGHT_CLOTH = partdefinition.addOrReplaceChild("RIGHT_CLOTH", CubeListBuilder.create()
+                .texOffs(10, 0).addBox(-16.0F, 0.0F, -11.0F, 16.0F, 0.0F, 22.0F, CubeDeformation.NONE)
+                .texOffs(32, 48).addBox(-15.0F, 0.0F, -11.0F, 14.0F, 2.0F, 0.0F, CubeDeformation.NONE)
+                .texOffs(32, 50).addBox(-11.0F, 0.0F, 11.0F, 10.0F, 2.0F, 0.0F, CubeDeformation.NONE),
+                PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, 0.0F, 0.0F, -0.1745F));
+
+        PartDefinition RIGHT_CLOTH_STREAMER = partdefinition.addOrReplaceChild("RIGHT_CLOTH_STREAMER", CubeListBuilder.create()
+                .texOffs(16, 22).addBox(-11.0F, 0.0F, 0.0F, 10.0F, 0.0F, 16.0F, CubeDeformation.NONE),
+                PartPose.offsetAndRotation(0.0F, 0.0F, 11.0F, -0.1745F, 0.0F, 0.0F));
+
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
 
@@ -79,13 +131,33 @@ public class GliderLayer extends RenderLayer<LivingEntity, HumanoidModel<LivingE
             }
 
             VertexConsumer frameVertexconsumer = multiBufferSource.getBuffer(RenderType.entityCutoutNoCull(FRAME_TEXTURE));
-            this.frameRoot.zRot = 0.8F * zRadians;
-            this.frameRoot.x = -6 * zRadians;
-			this.frameRoot.render(poseStack, frameVertexconsumer, i, OverlayTexture.NO_OVERLAY);
-
             VertexConsumer clothVertexconsumer = multiBufferSource.getBuffer(RenderType.entityCutoutNoCull(clothTexture));
-            this.clothRoot.copyFrom(this.frameRoot);
-			this.clothRoot.render(poseStack, clothVertexconsumer, i, OverlayTexture.NO_OVERLAY);
+
+            this.LEFT_FRAME.zRot = 0.8F * zRadians;
+            this.LEFT_FRAME.x = -6 * zRadians;
+            this.LEFT_FRAME.y = -32;
+            this.RIGHT_FRAME.copyFrom(this.LEFT_FRAME);
+            this.LEFT_CLOTH.copyFrom(this.LEFT_FRAME);
+            this.RIGHT_CLOTH.copyFrom(this.LEFT_FRAME);
+            this.LEFT_CLOTH_STREAMER.copyFrom(this.LEFT_FRAME);
+            this.RIGHT_CLOTH_STREAMER.copyFrom(this.LEFT_FRAME);
+
+            this.LEFT_FRAME.zRot += 0.1F;
+            this.RIGHT_FRAME.zRot -= 0.1F;
+            this.LEFT_CLOTH.zRot += 0.1F;
+            this.RIGHT_CLOTH.zRot -= 0.1F;
+            this.LEFT_CLOTH_STREAMER.zRot += 0.1F;
+            this.LEFT_CLOTH_STREAMER.xRot = 100;
+            this.RIGHT_CLOTH_STREAMER.zRot -= 0.1F;
+            this.RIGHT_CLOTH_STREAMER.xRot = 100;
+
+			this.LEFT_FRAME.render(poseStack, frameVertexconsumer, i, OverlayTexture.NO_OVERLAY);
+            this.RIGHT_FRAME.render(poseStack, frameVertexconsumer, i, OverlayTexture.NO_OVERLAY);
+
+            this.LEFT_CLOTH.render(poseStack, clothVertexconsumer, i, OverlayTexture.NO_OVERLAY);
+            this.RIGHT_CLOTH.render(poseStack, clothVertexconsumer, i, OverlayTexture.NO_OVERLAY);
+            this.LEFT_CLOTH_STREAMER.render(poseStack, clothVertexconsumer, i, OverlayTexture.NO_OVERLAY);
+            this.RIGHT_CLOTH_STREAMER.render(poseStack, clothVertexconsumer, i, OverlayTexture.NO_OVERLAY);
         }
     }
 }
